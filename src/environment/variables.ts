71fccf21ad69454ed827b59c41005a1492f31881
moontai0724 <moontai0724@gmail.com>
2024-01-22 22:33:45 +0800

    feat: environment variable

---
+++ src/environment/variables.ts
@@ -0,0 +1,49 @@
+import recursiveProxyUtil from "../utils/recursive-proxy";
+
+/**
+ * Defines the environment variables here. The key of first level object will be
+ * the identifier that stored into the script storage, and the value will be the
+ * default value if the value is not set before.
+ */
+const environmentVariables = {
+  enable: {
+    /**
+     * Whether to enable the daily sign feature.
+     * @default true
+     */
+    dailySign: true,
+  },
+};
+
+/**
+ * A proxy object that will read and write the environment variables from the
+ * script storage. The value will be the default value if the value is not set
+ * in the script storage.
+ */
+const environment = new Proxy(
+  environmentVariables as Record<number | string | symbol, unknown>,
+  {
+    get(target, key) {
+      const value = GM_getValue(key.toString(), target[key]);
+
+      if (typeof value === "object" && value !== null) {
+        return new Proxy(
+          value,
+          recursiveProxyUtil.getOptions(
+            environment as unknown as Record<number | string | symbol, object>,
+            key,
+          ),
+        );
+      }
+
+      return value;
+    },
+    set(_, name, value) {
+      GM_setValue(name.toString(), value);
+
+      return true;
+    },
+  },
+) as typeof environmentVariables;
+
+export default environment;
---
+++ src/utils/recursive-proxy.ts
@@ -0,0 +1,63 @@
+/* eslint-disable no-param-reassign, max-lines-per-function */
+/**
+ * Get a recursive proxy option for the proxy object, which will auto trigger
+ * parent setter when the child setter is triggered.
+ * @param parentTarget parent of this item
+ * @param parentKey key of this item in the parent
+ * @returns a recursive proxy option for the proxy object
+ */
+function getOptions<
+  T extends Record<number | string | symbol, object>,
+  K extends keyof T,
+>(parentTarget: T, parentKey: K): ProxyHandler<T[K]> {
+  const handler: ProxyHandler<T[K]> = {
+    get(itemTarget, itemKey) {
+      // type the variables
+      type ItemTarget = typeof itemTarget;
+      type ItemKeys = keyof ItemTarget;
+      const typedItemKey = itemKey as ItemKeys;
+
+      // get current item
+      const itemValue = itemTarget[typedItemKey];
+
+      // if the item is an object, return a proxy
+      if (typeof itemValue === "object" && itemValue !== null) {
+        return new Proxy(
+          itemValue,
+          getOptions(
+            new Proxy(itemTarget, handler) as Record<
+              number | string | symbol,
+              object
+            >,
+            itemKey,
+          ),
+        );
+      }
+
+      // return the item
+      return itemValue;
+    },
+    set(itemTarget, itemKey, itemValue) {
+      // type the variables
+      type ItemTarget = typeof itemTarget;
+      type ItemKeys = keyof ItemTarget;
+      const typedItemKey = itemKey as ItemKeys;
+      const typedItemValue = itemValue as ItemTarget[ItemKeys];
+
+      // update current item
+      itemTarget[typedItemKey] = typedItemValue;
+
+      // trigger parent setter
+      parentTarget[parentKey] = itemTarget;
+
+      // report success
+      return true;
+    },
+  };
+
+  return handler;
+}
+
+const recursiveProxyUtil = { getOptions };
+
+export default recursiveProxyUtil;
