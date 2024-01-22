183a7e561bb17f4a1a44eb7c747d5871be42733a
moontai0724 <moontai0724@gmail.com>
2024-01-22 22:25:53 +0800

    feat(api): daily sign

---
+++ src/apis/daily-sign.ts
@@ -0,0 +1,93 @@
+import CSRFTokenService from "services/csrf-token";
+import httpService, { HeaderOption } from "services/http";
+
+export interface ApiDailySignCheckResult {
+  btnMessage: string;
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  close_auto: number;
+  days: number;
+  dialogInfo: string[];
+  finishedAd: number;
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  open_auto_ani: number;
+  prjSigninDays: number;
+  signin: number;
+  totalWeeks: number;
+}
+
+/**
+ * This is the api that the current landing page uses.
+ */
+async function check(): Promise<ApiDailySignCheckResult> {
+  const token = CSRFTokenService.getCSRFToken();
+
+  const params = new FormData();
+
+  params.append("action", "2");
+
+  const response = await httpService.post<{ data: ApiDailySignCheckResult }>(
+    "https://api.gamer.com.tw/user/v1/signin.php",
+    {
+      anonymous: false,
+      cookie: `ckBahamutCsrfToken=${token}`,
+      data: params as unknown as string,
+      headers: {
+        [HeaderOption.XBahamutCsrfToken]: token,
+      },
+      responseType: "json",
+    },
+  );
+
+  if ("error" in response) return Promise.reject(response.error);
+
+  return response.data;
+}
+
+interface ApiSignResult {
+  btnMessage: string;
+  days: number;
+  dialog: string;
+  dialogInfo: [];
+  prjSigninDays: number;
+  totalWeeks: number;
+}
+
+/**
+ * This is the api that the current landing page uses.
+ * @example {
+    "days": 111,
+    "dialog": "",
+    "prjSigninDays": 0,
+    "btnMessage": "<i class=\"material-icons\">check_box</i>每日簽到已達成",
+    "totalWeeks": 4,
+    "dialogInfo": []
+  }
+ */
+async function sign(): Promise<ApiSignResult> {
+  const token = CSRFTokenService.getCSRFToken();
+
+  const params = new FormData();
+
+  params.append("action", "1");
+
+  const response = await httpService.post<{ data: ApiSignResult }>(
+    "https://api.gamer.com.tw/user/v1/signin.php",
+    {
+      anonymous: false,
+      cookie: `ckBahamutCsrfToken=${token}`,
+      data: params as unknown as string,
+      headers: {
+        [HeaderOption.XBahamutCsrfToken]: token,
+      },
+      responseType: "json",
+    },
+  );
+
+  if ("error" in response) return Promise.reject(response.error);
+
+  return response.data;
+}
+
+const dailySignApi = { check, sign };
+
+export default dailySignApi;
---
+++ src/services/csrf-token.ts
@@ -0,0 +1,44 @@
+/**
+ * Generate a random hexadecimal string
+ *
+ * @param length The length of the hexadecimal string
+ */
+function generateRandomHex(length = 16): string {
+  // Initialize an empty string to store the hexadecimal value
+  let hexString = "";
+
+  // Loop through the length of the hexadecimal string
+  // eslint-disable-next-line no-plusplus
+  for (let index = 0; index < length; index++) {
+    // Generate a random hexadecimal digit (0-9, A-F)
+    const randomDigit = Math.floor(Math.random() * 16).toString(16);
+
+    // Append the random digit to the hexadecimal string
+    hexString += randomDigit;
+  }
+
+  // Return the generated hexadecimal string
+  return hexString;
+}
+
+/**
+ * Get the CSRF token from `ckBahamutCsrfToken` cookie or generate a random one.
+ *
+ * @returns The CSRF token from cookie or a random 16-digit hexadecimal string
+ */
+function getCSRFToken() {
+  const existing = document.cookie
+    .split(";")
+    .find(value => value.trim().startsWith("ckBahamutCsrfToken"));
+
+  if (!existing) return generateRandomHex();
+
+  const [, ...fragments] = existing.split("=");
+  const value = fragments.join("=");
+
+  return value;
+}
+
+const csrfTokenService = { getCSRFToken };
+
+export default csrfTokenService;
