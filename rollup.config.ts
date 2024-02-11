ec0413e09fe1b18a78fe5ef0a4640eca63f64fc8
moontai0724 <moontai0724@gmail.com>
2024-02-11 14:59:41 +0800

    feat: load view as resources

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,14 +1,60 @@
 import alias from "@rollup/plugin-alias";
 import typescript from "@rollup/plugin-typescript";
 import { resolve } from "path";
-import html from "rollup-plugin-html";
-import license from "rollup-plugin-license";
+
+import externalResource, {
+  getResourceMapping,
+} from "./scripts/external-resource";
 
 const root = "src/app";
 
 export default {
   input: `${root}/index.ts`,
   output: {
+    banner: () => {
+      // eslint-disable-next-line global-require
+      const meta = require("./package.json");
+      const scriptMeta = meta["user-script-meta"] || {};
+
+      scriptMeta.version = meta.version;
+      if (!scriptMeta.name && meta.name) scriptMeta.name = meta.name;
+      if (!scriptMeta.namespace && meta.homepage)
+        scriptMeta.namespace = meta.homepage;
+      if (!scriptMeta.description && meta.description)
+        scriptMeta.description = meta.description;
+      if (!scriptMeta.author && (meta.author?.name || meta.author))
+        scriptMeta.author = meta.author?.name || meta.author;
+      if (!scriptMeta.homepage && meta.homepage)
+        scriptMeta.homepage = meta.homepage;
+      if (!scriptMeta.supportURL && (meta.bugs?.url || meta.homepage))
+        scriptMeta.supportURL = meta.bugs.url || meta.homepage;
+
+      scriptMeta.resource = getResourceMapping({
+        baseUrl: scriptMeta.homepage,
+      });
+
+      const metaString = Object.entries(scriptMeta)
+        .map(([metaKey, metaValue]) => {
+          function getMetaString(key, value) {
+            return `// @${key.padEnd(15)} ${value}`;
+          }
+
+          if (Array.isArray(metaValue)) {
+            return metaValue.map(v => getMetaString(metaKey, v)).join("\n");
+          }
+
+          if (typeof metaValue === "object") {
+            return Object.entries(metaValue)
+              .map(([k, v]) => getMetaString(`${metaKey}:${k}`, v))
+              .join("\n");
+          }
+
+          return getMetaString(metaKey, metaValue);
+        })
+        .join("\n");
+
+      return `// ==UserScript==\n${metaString}\n// ==/UserScript==\n`;
+    },
     file: "dist/bundle.user.js",
     format: "umd",
     minifyInternalExports: false,
@@ -23,59 +69,8 @@ export default {
     typescript({
       tsconfig: `${root}/tsconfig.json`,
     }),
-    html({
-      htmlMinifierOptions: {
-        collapseBooleanAttributes: true,
-        collapseWhitespace: true,
-        conservativeCollapse: true,
-        minifyJS: true,
-      },
-      include: "**/*.html",
-    }),
-    license({
-      banner: {
-        commentStyle: "none",
-        content: (() => {
-          // eslint-disable-next-line global-require
-          const meta = require("./package.json");
-          const scriptMeta = meta["user-script-meta"] || {};
-
-          scriptMeta.version = meta.version;
-          if (!scriptMeta.name && meta.name) scriptMeta.name = meta.name;
-          if (!scriptMeta.namespace && meta.homepage)
-            scriptMeta.namespace = meta.homepage;
-          if (!scriptMeta.description && meta.description)
-            scriptMeta.description = meta.description;
-          if (!scriptMeta.author && (meta.author?.name || meta.author))
-            scriptMeta.author = meta.author?.name || meta.author;
-          if (!scriptMeta.homepage && meta.homepage)
-            scriptMeta.homepage = meta.homepage;
-          if (!scriptMeta.supportURL && (meta.bugs?.url || meta.homepage))
-            scriptMeta.supportURL = meta.bugs.url || meta.homepage;
-
-          const metaString = Object.entries(scriptMeta)
-            .map(([metaKey, metaValue]) => {
-              function getMetaString(key, value) {
-                return `// @${key.padEnd(15)} ${value}`;
-              }
-
-              if (Array.isArray(metaValue)) {
-                return metaValue.map(v => getMetaString(metaKey, v)).join("\n");
-              }
-
-              if (typeof metaValue === "object") {
-                return Object.entries(metaValue)
-                  .map(([k, v]) => getMetaString(`${metaKey}:${k}`, v))
-                  .join("\n");
-              }
-
-              return getMetaString(metaKey, metaValue);
-            })
-            .join("\n");
-
-          return `// ==UserScript==\n${metaString}\n// ==/UserScript==\n`;
-        })(),
-      },
+    externalResource({
+      production: process.env.NODE_ENV === "production",
     }),
   ],
 };
---
+++ scripts/external-resource.ts
@@ -0,0 +1,146 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { createFilter } from "@rollup/pluginutils";
+import htmlMinifier, {
+  type Options as HtmlMinifierOptions,
+} from "html-minifier";
+import Path from "path";
+
+export const resources = new Map();
+
+interface PluginOptions {
+  exclude?: string;
+  htmlMinifierOptions?: HtmlMinifierOptions;
+  include?: string;
+  /**
+   * Whether to use external resource.
+   */
+  production?: boolean;
+}
+
+function getExternalTransformMethod(options: PluginOptions) {
+  const include = options.include || "**/*.ts";
+  const filter = createFilter(include, options.exclude);
+
+  function transform(
+    sourceCode: string,
+    importPath: string,
+  ): { code: string; map: { mappings: string } } | void {
+    if (filter(importPath)) return;
+
+    const resource = (() => {
+      const relativePath = Path.relative(
+        Path.join(__dirname, "dist"),
+        importPath,
+      );
+      const existing = resources.get(relativePath);
+
+      if (existing) return existing;
+
+      const random = Math.random().toString(36).substring(7);
+      const name = `resource_${random}`;
+
+      resources.set(relativePath, name);
+
+      return name;
+    })();
+
+    const code = `export default ${resource};`;
+
+    // eslint-disable-next-line consistent-return
+    return {
+      code,
+      map: { mappings: "" },
+    };
+  }
+
+  return transform;
+}
+
+function getInlineTransformMethod(options: PluginOptions) {
+  const include = options.include || "**/*.ts";
+  const filter = createFilter(include, options.exclude);
+
+  function transform(
+    sourceCode: string,
+    importId: string,
+  ): { code: string; map: { mappings: string } } | void {
+    if (filter(importId)) return;
+
+    const minified = JSON.stringify(
+      htmlMinifier.minify(sourceCode, options.htmlMinifierOptions),
+    );
+
+    const code = `export default ${minified};`;
+
+    // eslint-disable-next-line consistent-return
+    return {
+      code,
+      map: { mappings: "" },
+    };
+  }
+
+  return transform;
+}
+
+function initPlugin(options: PluginOptions) {
+  const baseOptions = {
+    name: "external-resource",
+  };
+  const { production } = options;
+
+  if (!production) {
+    Object.assign(baseOptions, {
+      transform: getInlineTransformMethod(options),
+    });
+  } else {
+    Object.assign(baseOptions, {
+      transform: getExternalTransformMethod(options),
+    });
+  }
+
+  return baseOptions;
+}
+
+interface ScriptCommentOptions {
+  /**
+   * Base URL of the GitHub repository where the user-script is hosted, used to
+   * generate raw URL of resources.
+   */
+  baseUrl: string;
+  /**
+   * Path based on the root of the repository.
+   */
+  path?: string;
+}
+
+/**
+ * Get resource mapping for user-script.
+ *
+ * @param param0 options when generating resource mapping.
+ * @example resource_5jqnq8 https://github.com/moontai0724/bahamut-sign-helper-script/raw/pages/animad-manual-answer/index.html
+ */
+export function getResourceMapping({
+  baseUrl,
+  path = "/raw/release/",
+}: ScriptCommentOptions) {
+  if (!/^https:\/\/github.com\/\w+?\/\w+?/.test(baseUrl)) {
+    throw new Error(`baseUrl must be a GitHub repository: ${baseUrl}`);
+  }
+
+  return Array.from(resources.entries()).map(([relativePath, resource]) => {
+    const result = `${baseUrl}${path}${relativePath}`;
+
+    console.log(`Resource: ${resource} -> ${result}`);
+
+    return `${resource} ${result}`;
+  });
+}
+
+/**
+ * Replace imports by using `@resource` of user-script.
+ *
+ * @see https://www.tampermonkey.net/documentation.php#meta:resource
+ */
+export default function externalResource(options: PluginOptions = {}) {
+  return initPlugin(options);
+}
